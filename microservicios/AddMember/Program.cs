caf2e02ad6780169d7162aa12d997a1388ea3151
josshare <36396116+josshare@users.noreply.github.com>
2025-07-29 01:05:24 -0600

    children and adult working

--- microservicios/AddMember/Program.cs
+++ microservicios/AddMember/Program.cs
@@ -1,41 +1,31 @@
+using AddMember.Data;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Swagger;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+app.MapPost("/addmember", (string name, string lastname, string birthyear) =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    var connectionString = builder.Configuration["ServiceBus:ConnectionString"];
+    var queueName = builder.Configuration["ServiceBus:QueueName"];
+    var serviceBus = new ServiceBus(connectionString, queueName);
+    serviceBus.SendMessageAsync(name, lastname, birthyear).GetAwaiter().GetResult();
+    return Results.Ok($"Miembro {name} agregado con éxito.");
 })
-.WithName("GetWeatherForecast");
+.WithName("AddMember")
+.WithOpenApi();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
